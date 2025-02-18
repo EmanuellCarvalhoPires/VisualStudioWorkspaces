398a80c6bf16637f18810d55ab7157085879f356
Emanuell Carvalho <72475145+EmanuellCarvalhoPires@users.noreply.github.com>
2025-02-18 10:46:37 -0300

    Mais detalhes

--- First SPA - React/vite-project/src/assets/ModalFalha.jsx
+++ First SPA - React/vite-project/src/assets/ModalFalha.jsx
@@ -3,7 +3,8 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import PropTypes from "prop-types"
 import Form from 'react-bootstrap/Form';
-
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 function Example({ ticket }) {
   const [show, setShow] = useState(false);
   
@@ -15,35 +16,56 @@ function Example({ ticket }) {
       <Button variant="dark" onClick={() => setShow(true)}>
         Detalhes do Chamado
       </Button>
-
+    
       <Modal
         size='lg'
+        id="ModalTudo"
+        
         show={show}
         onHide={() => setShow(false)}
         dialogClassName="modal-90w"
         aria-labelledby="example-custom-modal-styling-title"
       >
-        <Modal.Header closeButton>
+        
+        <Modal.Header closeButton id='ModalHeader'>
           <Modal.Title id="example-custom-modal-styling-title">
             Ticket: {ticket.ticket}
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <p id="overflow"><strong>MVNO: {ticket.mvno}</strong>  <strong>Prioridade: {ticket.prioridade}</strong> </p>
-          
-          <p id="overflow"><strong>Descrição:</strong> {ticket.descricao}</p>
 
-          <Form>
-            <Form.Group
+        <Tabs defaultActiveKey="home1" id="uncontrolled-tab-example" className="mb-3">
+          
+                <Tab eventKey="home1" title="Descrição">
+                    
+                    <p id="overflow" className='TituloModal'><strong>MVNO: {ticket.mvno}</strong> <strong>Setor: {ticket.donofalha}</strong> <strong>Prioridade: {ticket.prioridade}</strong> </p>  
+                    <Modal.Body id="ModalBody">
+                      
+                      <p id="overflow"><strong>Descrição:</strong> {ticket.descricao}</p>
+                    </Modal.Body>
+
+                </Tab>
+
+
+
+                <Tab eventKey="profile1" title="Comentários">
+                  <Form id="comentários">
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    
+                    <Form.Label>Comentários</Form.Label>
+                    <Form.Control as="textarea" rows={3}/>
+
+                  </Form.Group>
+                </Form>
+                </Tab>
+
               
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Comentários</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
+
+                <Tab eventKey="contact1" title="Extras">
+                  Tab content for Contact
+                </Tab>
+
+        </Tabs>
+
       </Modal>
     </>
   );
